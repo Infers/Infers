b603cccf70abefab7e5faf382306c8662bdeede5
Vesa Karvonen <vesa.a.j.k@gmail.com>
2014-05-04 16:05:48 +0300

    Helps to actually backtrack.  Must review when properly awake. :)

--- proto/Infers/Infers/Engine.fs
+++ proto/Infers/Infers/Engine.fs
@@ -57,73 +57,77 @@ module InfRuleSet =
 
 [<AutoOpen>]
 module IDDFS =
-  let (>>=) xO x2yO = Option.bind x2yO xO
-  let (|>>) xO x2y = Option.map x2y xO
-  let guard b = if b then Some () else None
+  let result x = Seq.singleton x
+  let (>>=) xO x2yO = Seq.collect x2yO xO
+  let (|>>) xO x2y = Seq.map x2y xO
+  let guard b = if b then result () else Seq.empty
 
   let rec dfsGenerate (explain: bool)
                       (nesting: int)
                       (limit: int)
                       (infRuleSet: HashEqSet<obj>)
                       (knownObjs: HashEqMap<Type, unit -> obj>)
-                      (desiredTy: Type) : option<_ * _ * _> =
+                      (desiredTy: Type) : seq<_ * _ * _> =
     let inline tell u2msg =
       if explain then
         printfn "%s%s" (String.replicate nesting " ") (u2msg ())
     tell <| fun () -> sprintf "desired: %A" desiredTy
-    match HashEqMap.tryFind desiredTy knownObjs with
-     | Some u2o ->
+    match HashEqMap.tryFind desiredTy knownObjs
+          |> Option.bind (fun u2o ->
+             try Some (u2o ()) with Backtrack -> None) with
+     | Some o ->
        tell <| fun () -> sprintf "known"
-       Some (u2o (), HashEqMap.empty, knownObjs)
+       result (o, HashEqMap.empty, knownObjs)
      | None ->
+       guard (nesting < limit) >>= fun () ->
        let nesting = nesting + 1
-       guard (nesting <= limit) >>= fun () ->
-       InfRuleSet.rules infRuleSet
-       |> Seq.tryPick (fun infRule ->
-          tell <| fun () -> sprintf "trying: %A :- %A" infRule.ReturnType infRule.ParTypes
-          tryMatch infRule.ReturnType desiredTy HashEqMap.empty >>= fun v2t ->
-          let desiredTy = resolve v2t desiredTy
-          guard (not (containsVars desiredTy)) >>= fun () ->
-          tell <| fun () -> sprintf "match as: %A" desiredTy
-          let recTy = typedefof<Rec<_>>.MakeGenericType [|desiredTy|]
-          let (knownObjs, tie) =
-            match dfsGenerate explain nesting limit infRuleSet knownObjs recTy with
-             | None ->
-               (HashEqMap.add desiredTy
-                 (fun () -> failwithf "Error: No rec rule for %A" desiredTy)
-                 knownObjs,
-                K >> HashEqMap.add desiredTy)
-             | Some (recObj, _, knownObjs) ->
-               let recObj = recObj :?> RecObj
-               (HashEqMap.add desiredTy
-                 (fun () -> recObj.GetObj ()) knownObjs,
-                fun complete knownObjs ->
-                  recObj.SetObj complete
-                  HashEqMap.add desiredTy (K complete) knownObjs)
-          let rec lp infRuleSet genArgTypes knownObjs argObjs parTypes =
-            match parTypes with
-             | [] ->
-               let argObjs = Array.ofList (List.rev argObjs)
-               infRule.Invoke (genArgTypes, argObjs) |>> fun desiredObj ->
+       InfRuleSet.rules infRuleSet >>= fun infRule ->
+       tell <| fun () -> sprintf "trying: %A :- %A" infRule.ReturnType infRule.ParTypes
+       tryMatch infRule.ReturnType desiredTy HashEqMap.empty |> Option.toSeq >>= fun v2t ->
+       let desiredTy = resolve v2t desiredTy
+       guard (not (containsVars desiredTy)) >>= fun () ->
+       tell <| fun () -> sprintf "match as: %A" desiredTy
+       let recTy = typedefof<Rec<_>>.MakeGenericType [|desiredTy|]
+       let (knownObjs, tie) =
+         match dfsGenerate explain nesting limit infRuleSet knownObjs recTy
+               |> Seq.tryPick Some with
+          | None ->
+            (HashEqMap.add desiredTy
+              (fun () -> raise Backtrack)
+              knownObjs,
+             K >> HashEqMap.add desiredTy)
+          | Some (recObj, _, knownObjs) ->
+            let recObj = recObj :?> RecObj
+            (HashEqMap.add desiredTy
+              (fun () -> recObj.GetObj ()) knownObjs,
+             fun complete knownObjs ->
+               recObj.SetObj complete
+               HashEqMap.add desiredTy (K complete) knownObjs)
+       let rec lp infRuleSet genArgTypes knownObjs argObjs parTypes =
+         match parTypes with
+          | [] ->
+            let argObjs = Array.ofList (List.rev argObjs)
+            infRule.Invoke (genArgTypes, argObjs)
+            |> Option.toSeq |>> fun desiredObj ->
                tell <| fun () -> sprintf "built: %A" desiredTy
                (desiredObj, v2t, tie desiredObj knownObjs)
-             | parType::parTypes ->
-               dfsGenerate explain nesting limit infRuleSet knownObjs parType >>=
-               fun (argObj, v2t, knownObjs) ->
-                 lp (InfRuleSet.maybeAddRules argObj infRuleSet)
-                    (genArgTypes |> Array.map (resolve v2t))
-                    knownObjs
-                    (argObj::argObjs)
-                    (parTypes |> List.map (resolve v2t))
-          lp infRuleSet
-             (infRule.GenericArgTypes |> Array.map (resolve v2t))
-             knownObjs
-             []
-             (infRule.ParTypes |> Array.map (resolve v2t) |> List.ofArray))
+          | parType::parTypes ->
+            dfsGenerate explain nesting limit infRuleSet knownObjs parType >>=
+             fun (argObj, v2t, knownObjs) ->
+               lp (InfRuleSet.maybeAddRules argObj infRuleSet)
+                  (genArgTypes |> Array.map (resolve v2t))
+                  knownObjs
+                  (argObj::argObjs)
+                  (parTypes |> List.map (resolve v2t))
+       lp infRuleSet
+          (infRule.GenericArgTypes |> Array.map (resolve v2t))
+          knownObjs
+          []
+          (infRule.ParTypes |> Array.map (resolve v2t) |> List.ofArray)
 
   let iddfsGenerate explain initialDepth maxDepth infRuleSet desiredTy =
     seq {initialDepth .. maxDepth}
-    |> Seq.tryPick (fun limit ->
+    |> Seq.collect (fun limit ->
        dfsGenerate explain 0 limit infRuleSet HashEqMap.empty desiredTy)
 
 type [<Sealed>] Engine =
@@ -139,8 +143,9 @@ type [<Sealed>] Engine =
      initialDepth
      maxDepth
      (InfRuleSet.ofSeq rules)
-     typeof<'a> |>> fun (x, _, _) ->
-      unbox<'a> x
+     typeof<'a>
+    |> Seq.tryPick (fun (x, _, _) ->
+       Some (unbox<'a> x))
 
   static member TryGenerate (rules: obj) =
     Engine.TryGenerate (false, Int32.MaxValue, Int32.MaxValue, [rules])
