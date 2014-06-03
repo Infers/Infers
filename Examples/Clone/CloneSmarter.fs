692baf76bb01552ea13c10bf31579580f07e6551
Vesa Karvonen <vesa.a.j.k@gmail.com>
2014-06-03 16:42:06 +0300

    Somewhat smarter now to avoid copying recursive types if the only thing they have to copy is the type itself.  This is a silly workaround.  A smarter approach would be to implement a generic function to analyze types to tell whether they include any mutable elements, like I did years ago in my SML generics library.

--- Examples/Clone/CloneSmarter.fs
+++ Examples/Clone/CloneSmarter.fs
@@ -9,36 +9,42 @@ open Infers.Rep
 /// Type of cloning functions.
 type Clone<'x> = 'x -> 'x
 
-/// A smart cloning function.  We allow null, which means that there is nothing to copy.
-type [<AllowNullLiteral; AbstractClass>] CloneSmart<'x> () =
+/// A smart cloning function.
+type [<AllowNullLiteral; AbstractClass>] CloneSmarter<'x> () =
   abstract Clone: 'x -> 'x
 
-/// Type of an intermediate step in cloning a product type.  We allow null, which means
-/// that there is nothing to copy.
+/// Type of an intermediate step in cloning a product type.
 type [<AllowNullLiteral; AbstractClass>] CloneProduct<'e, 'es, 't> () =
   abstract Clone: byref<'e> -> unit
+  abstract ForAll: (obj -> bool) -> bool
 
 /// Type of a partial union cloning function (or list of such functions).
-type CloneUnion<'c, 'cs, 't> = CloneUnion of list<CloneSmart<'t>>
+type CloneUnion<'c, 'cs, 't> = CloneUnion of list<CloneSmarter<'t>>
 
 ////////////////////////////////////////////////////////////////////////////////
  
 /// A helper function for products (including individual union cases).
-let inline CloneProductToCloneSmart (asProduct: AsProduct<'es, 't>)
-                                    (cloneProduct: CloneProduct<'es, 'es, 't>) : CloneSmart<'t> =
-  match cloneProduct with
-   | null -> null
+let CloneProductToCloneSmart (isMutable: bool)
+                             (asProduct: AsProduct<'es, 't>)
+                             (cloneProduct': CloneProduct<'es, 'es, 't>) : CloneSmarter<'t> =
+  match cloneProduct' with
+   | null ->
+     null
    | cloneProduct ->
-     {new CloneSmart<'t> () with
-       override this.Clone (original) =
-        // First we allocate space for elements of the clone from stack.
-        let mutable elems = Unchecked.defaultof<'es>
-        // We then extract the original elements of the product.
-        asProduct.Extract (original, &elems)
-        // Then we clone the elements in-place.
-        cloneProduct.Clone (&elems)
-        // Then we create a clone from the cloned elements.
-        asProduct.Create (&elems)}
+     if not isMutable &&
+        cloneProduct.ForAll (function :? CloneSmarter<'t> -> true | _ -> false) then
+       null
+     else
+       {new CloneSmarter<'t> () with
+         override this.Clone (original) =
+          // First we allocate space for elements of the clone from stack.
+          let mutable elems = Unchecked.defaultof<'es>
+          // We then extract the original elements of the product.
+          asProduct.Extract (original, &elems)
+          // Then we clone the elements in-place.
+          cloneProduct.Clone (&elems)
+          // Then we create a clone from the cloned elements.
+          asProduct.Create (&elems)}
 
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -46,7 +52,7 @@ let inline CloneProductToCloneSmart (asProduct: AsProduct<'es, 't>)
 type [<InferenceRules>] Clone () =
   // Turn a smart clone to a simple clone --------------------------------------
 
-  member this.AsClone (clone: CloneSmart<'x>) : Clone<'x> =
+  member this.AsClone (clone: CloneSmarter<'x>) : Clone<'x> =
     match clone with
      | null  -> fun x -> x
      | clone -> fun x -> clone.Clone (x)
@@ -54,49 +60,49 @@ type [<InferenceRules>] Clone () =
   // Recursion rule ------------------------------------------------------------
 
   // Rule for creating a proxy when defining a recursive cloning function.
-  member this.Fix (recFn: RecFn) : Rec<CloneSmart<'t>> =
-    let proxy : ref<CloneSmart<'t>> = ref null
+  member this.Fix (recFn: RecFn) : Rec<CloneSmarter<'t>> =
+    let proxy : ref<CloneSmarter<'t>> = ref null
     let clone =
-      {new CloneSmart<'t> () with
+      {new CloneSmarter<'t> () with
         override this.Clone (x) =
          match !proxy with
           | null -> x
           | clone -> clone.Clone (x)}
-    {new Rec<CloneSmart<'t>> () with
+    {new Rec<CloneSmarter<'t>> () with
       override this.Get () = clone
       override this.Set (clone) = proxy := clone}
 
   // Rules for simple base types -----------------------------------------------
 
   /// A rule for the `int32` type.
-  member this.Int : CloneSmart<int> = null  // Nothing to copy!
+  member this.Int : CloneSmarter<int> = null  // Nothing to copy!
 
   /// A rule for the `string` type.
-  member this.String : CloneSmart<string> = null // Nothing to copy!
+  member this.String : CloneSmarter<string> = null // Nothing to copy!
 
   // Rules for some specific type constructors ---------------------------------
 
   /// A rule for the `ref<'t>` type constructor.
-  member this.Ref (cloneT: CloneSmart<'t>) : CloneSmart<ref<'t>> =
-    match cloneT with
+  member this.Ref (cloneX': CloneSmarter<'t>) : CloneSmarter<ref<'t>> =
+    match cloneX' with
      | null ->
-       {new CloneSmart<ref<'t>> () with
+       {new CloneSmarter<ref<'t>> () with
          override this.Clone (original) =
           ref (!original)}
-     | cloneT ->
-       {new CloneSmart<ref<'t>> () with
+     | cloneX ->
+       {new CloneSmarter<ref<'t>> () with
          override this.Clone (original) =
-          ref (!original)}
+          ref (cloneX.Clone (!original))}
 
   /// A rule for the `array<'t>` type constructor.
-  member this.Array (cloneX: CloneSmart<'x>) : CloneSmart<array<'x>> =
-    match cloneX with
+  member this.Array (cloneX': CloneSmarter<'x>) : CloneSmarter<array<'x>> =
+    match cloneX' with
      | null ->
-       {new CloneSmart<array<'x>> () with
+       {new CloneSmarter<array<'x>> () with
          override this.Clone (original) =
           Array.copy original}
      | cloneX ->
-       {new CloneSmart<array<'x>> () with
+       {new CloneSmarter<array<'x>> () with
          override this.Clone (original) =
           Array.map (fun x -> cloneX.Clone (x)) original}
 
@@ -112,7 +118,7 @@ type [<InferenceRules>] Clone () =
       null
     else
       let identity =
-        {new CloneSmart<'t> () with 
+        {new CloneSmarter<'t> () with 
           override this.Clone (original) = original}
       let cloneUnion =
         Array.ofList cloneUnion
@@ -120,7 +126,7 @@ type [<InferenceRules>] Clone () =
            match case with
             | null -> identity
             | case -> case)
-      {new CloneSmart<'t> () with
+      {new CloneSmarter<'t> () with
         override this.Clone (original) =
           // First we extract the case index.
           let caseIndex = union.Tag original
@@ -140,10 +146,10 @@ type [<InferenceRules>] Clone () =
 
   /// A rule for cloning a non-nullary union case.
   member this.Case (case: Case<'ls, 'cs, 't>,
-                    cloneProduct: CloneProduct<'ls, 'ls, 't>) : CloneUnion<'ls, 'cs, 't> =
+                    cloneProduct': CloneProduct<'ls, 'ls, 't>) : CloneUnion<'ls, 'cs, 't> =
     // We forward here to a helper function to avoid duplicating code.
     let cloneCase =
-      CloneProductToCloneSmart (case :> AsProduct<'ls, 't>) cloneProduct
+      CloneProductToCloneSmart false (case :> AsProduct<'ls, 't>) cloneProduct'
     // Then we return just this case. 
     CloneUnion [cloneCase]
 
@@ -158,13 +164,13 @@ type [<InferenceRules>] Clone () =
   member this.Product (_: Rep,
                        product: Product<'t>,
                        asProduct: AsProduct<'es, 't>,
-                       cloneProduct: CloneProduct<'es, 'es, 't>) : CloneSmart<'t> =
+                       cloneProduct': CloneProduct<'es, 'es, 't>) : CloneSmarter<'t> =
     // Do we need to copy something?
-    match (product.IsMutable, cloneProduct) with
+    match (product.IsMutable, cloneProduct') with
      | (false, null) ->
        null
-     | _ ->
-       CloneProductToCloneSmart asProduct cloneProduct
+     | (isMutable, cloneProduct) ->
+       CloneProductToCloneSmart isMutable asProduct cloneProduct
 
   /// A rule for cloning a part of a product consisting of an `Elem: 'e` and
   /// `Rest: 'es`.
@@ -177,24 +183,33 @@ type [<InferenceRules>] Clone () =
      | (null, cloneProductRest) ->
        {new CloneProduct<And<'e, 'es>, And<'e, 'es>, 't> () with
          override this.Clone (ees: byref<And<'e, 'es>>) =
-          cloneProductRest.Clone (&ees.Rest)}
+          cloneProductRest.Clone (&ees.Rest)
+         override this.ForAll (pred) =
+          cloneProductRest.ForAll pred}
      | (cloneProductElem, null) ->
        {new CloneProduct<And<'e, 'es>, And<'e, 'es>, 't> () with
          override this.Clone (ees: byref<And<'e, 'es>>) =
-          cloneProductElem.Clone (&ees.Elem)}
+          cloneProductElem.Clone (&ees.Elem)
+         override this.ForAll (pred) =
+          cloneProductElem.ForAll pred}
      | (cloneProductElem, cloneProductRest) ->
        {new CloneProduct<And<'e, 'es>, And<'e, 'es>, 't> () with
          override this.Clone (ees: byref<And<'e, 'es>>) =
           cloneProductElem.Clone (&ees.Elem)
-          cloneProductRest.Clone (&ees.Rest)}
+          cloneProductRest.Clone (&ees.Rest)
+         override this.ForAll (pred) =
+          cloneProductElem.ForAll pred &&
+          cloneProductRest.ForAll pred}
 
   /// A rule for cloning a specific element of type `'e` within a product.
-  member this.Elem (_: Rep, _: Elem<'e, 'es, 't>, cloneElem: CloneSmart<'e>) =
-    match cloneElem with
+  member this.Elem (_: Rep, _: Elem<'e, 'es, 't>, cloneElem': CloneSmarter<'e>) =
+    match cloneElem' with
      | null ->
        null
      | cloneElem ->
        {new CloneProduct<'e, 'es, 't> () with
          override this.Clone (e: byref<'e>) =
           // We just mutate the element in-place within the stack allocated space.
-          e <- cloneElem.Clone (e)}
+          e <- cloneElem.Clone (e)
+         override this.ForAll (pred) =
+          pred cloneElem}
