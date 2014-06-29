53c896b2c44f60b901f536f0916d7cb685787e8b
Vesa Karvonen <vesa.a.j.k@gmail.com>
2014-06-29 23:04:20 +0300

    Boilerplate for Infers.Basic lib.

---
+++ Libs/Infers.Basic/Assembly.fs
@@ -0,0 +1,27 @@
+﻿namespace Infers.Basic
+
+open System.Reflection
+open System.Runtime.InteropServices
+
+[<AutoOpen>]
+module AssemblyInfo =
+  [<Literal>]
+  let Version = "0.0.0.0"
+
+[<assembly: AssemblyTitle("Infers.Basic")>]
+[<assembly: AssemblyDescription("Infers.Basic implements a number of basic datatype generic functions.")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("Infers.Basic")>]
+[<assembly: AssemblyCopyright("© Vesa Karvonen")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+[<assembly: ComVisible(false)>]
+
+[<assembly: Guid("24e2775d-dc15-46fd-86a9-18bbc0e38c79")>]
+
+[<assembly: AssemblyVersion(Version)>]
+[<assembly: AssemblyFileVersion(Version)>]
+
+()
--- Libs/Infers.Rep/Assembly.fs
+++ Libs/Infers.Rep/Assembly.fs
@@ -1,4 +1,4 @@
-﻿namespace Infers
+﻿namespace Infers.Rep
 
 open System.Reflection
 open System.Runtime.InteropServices
@@ -6,13 +6,13 @@ open System.Runtime.InteropServices
 [<AutoOpen>]
 module AssemblyInfo =
   [<Literal>]
-  let Version = "0.0.0.2"
+  let Version = "0.0.0.3"
 
 [<assembly: AssemblyTitle("Infers.Rep")>]
 [<assembly: AssemblyDescription("Infers.Rep implements rules for datatype generic programming with Infers.")>]
 [<assembly: AssemblyConfiguration("")>]
 [<assembly: AssemblyCompany("")>]
-[<assembly: AssemblyProduct("Infers")>]
+[<assembly: AssemblyProduct("Infers.Rep")>]
 [<assembly: AssemblyCopyright("© Vesa Karvonen and Anton Tayanovskyy")>]
 [<assembly: AssemblyTrademark("")>]
 [<assembly: AssemblyCulture("")>]
