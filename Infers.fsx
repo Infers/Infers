b028028593679da31348f112e5c7c51642076c13
Vesa Karvonen <vesa.a.j.k@gmail.com>
2014-09-11 19:29:09 +0300

    Mono friendly paths.

--- Infers.fsx
+++ Infers.fsx
@@ -1,9 +1,9 @@
 #I __SOURCE_DIRECTORY__ ;;
 
-#r "Libs\\Infers.Core\\bin\\Release\\Infers.Core.dll" ;;
-#r "Libs\\Infers\\bin\\Release\\Infers.dll" ;;
-#r "Libs\\Infers.Rep\\bin\\Release\\Infers.Rep.dll" ;;
-#r "Libs\\Infers.Basic\\bin\\Release\\Infers.Basic.dll" ;;
+#r "Libs/Infers.Core/bin/Release/Infers.Core.dll" ;;
+#r "Libs/Infers/bin/Release/Infers.dll" ;;
+#r "Libs/Infers.Rep/bin/Release/Infers.Rep.dll" ;;
+#r "Libs/Infers.Basic/bin/Release/Infers.Basic.dll" ;;
 
 open Infers.Rep ;;
 open Infers ;;
