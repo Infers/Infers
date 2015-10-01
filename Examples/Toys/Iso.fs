cff2f56018acdbc49fa16c611b2fa51c1a1f7d18
Vesa Karvonen <vesa.a.j.k@gmail.com>
2015-10-01 13:55:13 +0300

    Rule to compose isos.

--- Examples/Toys/Iso.fs
+++ Examples/Toys/Iso.fs
@@ -12,6 +12,9 @@ type Iso<'x, 'y> = ('x -> 'y) * ('y -> 'x)
 type [<InferenceRules>] Iso () =
   member g.Id () : Iso<'x, 'x> = (id, id)
   member g.Inv ((t, o): Iso<'x, 'y>) : Iso<'y, 'x> = (o, t)
+  member g.Compose ((x2y, y2x): Iso<'x, 'y>,
+                    (y2z, z2y): Iso<'y, 'z>) : Iso<'x, 'z> =
+    (x2y >> y2z, z2y >> y2x)
   member g.Pairs ((t, o): Iso<     'pairs,         'ands>)
                         : Iso<'e * 'pairs, And<'e, 'ands>> =
     ((fun (e, es) ->
