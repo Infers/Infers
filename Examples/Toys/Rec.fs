c7ac9cfc022bb56af1b86554bcc39a3227c1f8b0
Vesa Karvonen <vesa.a.j.k@gmail.com>
2015-10-04 22:06:20 +0300

    Generalized rec rules.

--- Examples/Toys/Rec.fs
+++ Examples/Toys/Rec.fs
@@ -5,13 +5,40 @@ module Toys.Rec
 open Infers
 open Infers.Rep
 
+type [<AbstractClass>] ProductRec<'e, 'r, 't> () =
+  abstract Get: byref<'e> -> unit
+  abstract Set: byref<'e> -> unit
+
+/// Rules for computing fixed points over products, single case union types
+/// (a.k.a newtypes) and functions.
 type [<InferenceRules>] Rec () =
   inherit RecFn ()
 
-  member r.Newtype (_: Rep,
-                    _: AsChoice<'c, 'u>,
-                    m: Case<'c, 'c, 'u>,
-                    cR: Rec<'c>) : Rec<'u> =
-    {new Rec<'u> () with
-      override uR.Get () = cR.Get () |> m.OfProduct
-      override uR.Set u = m.ToProduct u |> cR.Set}
+  member t.Elem (_: Elem<'e, 'r, 't>, eR: Rec<'e>) =
+    {new ProductRec<'e, 'r, 't> () with
+      override pR.Get e = e <- eR.Get ()
+      override pR.Set e = eR.Set e}
+
+  member t.Times (eR: ProductRec<'e, And<'e, 'r>, 't>,
+                  rR: ProductRec<'r, 'r , 't>) =
+    {new ProductRec<And<'e, 'r>, And<'e, 'r>, 't> () with
+      override pR.Get er = eR.Get (&er.Elem) ; rR.Get (&er.Rest)
+      override pR.Set er = eR.Set (&er.Elem) ; rR.Set (&er.Rest)}
+
+  member t.Product (_: Rep,
+                    m: AsProduct<'p, 't>,
+                    pR: ProductRec<'p, 'p, 't>) =
+    {new Rec<'t> () with
+      override tR.Get () =
+        let mutable p = Unchecked.defaultof<'p>
+        pR.Get (&p)
+        m.Create (&p)
+      override tR.Set t =
+        let mutable p = m.ToProduct t
+        pR.Set (&p)}
+
+  member t.Newtype (r: Rep,
+                    _: AsChoice<'p, 'u>,
+                    m: Case<'p, 'p, 'u>,
+                    pR: ProductRec<'p, 'p, 'u>) =
+    t.Product (r, m, pR)
