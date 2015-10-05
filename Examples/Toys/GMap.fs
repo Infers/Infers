f676a519dc3a8ad8a8c1818eb23fd02ef2986cd1
Vesa Karvonen <vesa.a.j.k@gmail.com>
2015-10-05 21:02:35 +0300

    Generalized to arbitrary sums and products.

--- Examples/Toys/GMap.fs
+++ Examples/Toys/GMap.fs
@@ -3,25 +3,51 @@
 module Toys.GMap
 
 open Infers
+open Infers.Rep
+open Toys.Rec
 
 type GM<'w, 'p> = ('p -> 'p) -> 'w -> 'w
 type GMap<'w, 'p> = {gm: GM<'w, 'p>}
+type PGM<'e, 'r, 'w, 'p> = PGM of GM<'e, 'p>
+type SGM<'c, 's, 'w, 'p> = SGM of list<GM<'w, 'p>>
 
 type [<InferenceRules>] GMap () =
-  member g.same ()                  : GMap<'p, 'p> = {gm = id}
-  member g.notSame (gm: GM<'w, 'p>) : GMap<'w, 'p> = {gm = gm}
+  member g.Same ()                                  = {gm = id}
+  member g.NotSame (_: Rep, _: Rec, gm: GM<'w, 'p>) = {gm = gm}
 
-  member g.int    () : GM<int,    'p> = fun _ -> id
-  member g.string () : GM<string, 'p> = fun _ -> id
-  member g.bool   () : GM<bool,   'p> = fun _ -> id
+  member g.Prim (_: Prim<'w>) : GM<'w, 'p> = fun _ -> id
 
-  member g.option (wG: GMap<'w, 'p>) : GM<option<'w>, 'p> = wG.gm >> Option.map
-  member g.pair (vG: GMap<'v, 'p>, wG: GMap<'w, 'p>) : GM<'v * 'w, 'p> =
-    fun p2p (v, w) -> (vG.gm p2p v, wG.gm p2p w)
-  member g.list (wG: GMap<'w, 'p>) : GM<list<'w>, 'p> =
-    wG.gm >> List.map
+  member g.Elem (_: Elem<'e, 'r, 'w>,
+                 eG: GMap<'e, 'p>) : PGM<'e, 'r, 'w, 'p> =
+    PGM eG.gm
 
-let gmap (p2p: 'p -> 'p) : 'w -> 'w = StaticRules<GMap>.Generate () p2p
+  member g.Times (PGM eG: PGM<    'e     , And<'e, 'r>, 'w, 'p>,
+                  PGM rG: PGM<        'r ,         'r , 'w, 'p>)
+                        : PGM<And<'e, 'r>, And<'e, 'r>, 'w, 'p> =
+    PGM <| fun p2p (And (e, r)) -> And (eG p2p e, rG p2p r)
+
+  member g.Product (m: AsProduct<'r, 'w>, PGM rG: PGM<'r, 'r, 'w, 'p>) =
+    fun p2p -> m.ToProduct >> rG p2p >> m.OfProduct
+
+  member g.Case (_: Case<Empty, 's, 'w>) : SGM<Empty, 's, 'w, 'p> =
+    SGM [fun _ -> id]
+
+  member g.Case (m: Case<'e, 's, 'w>,
+                 eG: PGM<'e, 'e, 'w, 'p>) : SGM<'e, 's, 'w, 'p> =
+    SGM [g.Product (m, eG)]
+
+  member g.Plus (SGM eG: SGM<       'e     , Choice<'e, 'r>, 'w, 'p>,
+                 SGM rG: SGM<           'r ,            'r , 'w, 'p>)
+                       : SGM<Choice<'e, 'r>, Choice<'e, 'r>, 'w, 'p> =
+    SGM (eG @ rG)
+
+  member g.Sum (m: Union<'w>,
+                _: AsChoice<'s, 'w>,
+                SGM sG: SGM<'s, 's, 'w, 'p>) =
+    let cs = Array.ofList sG
+    fun p2p w -> cs.[m.Tag w] p2p w
+
+let gmap (p2p: 'p -> 'p) : 'w -> 'w = StaticRules<GMap>.Generate().gm p2p
 
 let test () : unit =
   gmap ((+) 1) [("vesa", 1)] |> printfn "%A"
