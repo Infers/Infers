3419890fe621059de4b7c4028eacc47e7555512f
Vesa Karvonen <vesa.a.j.k@gmail.com>
2015-10-05 14:36:17 +0300

    Being more faitful to the Wikipedia article.

--- Examples/Toys/List.fs
+++ Examples/Toys/List.fs
@@ -13,34 +13,54 @@ type Cons<'h, 't> = | Cons
 
 /// Shorthand for a 1-element list.
 type List<'x0> = Cons<'x0, Nil>
+
 /// Shorthand for a 2-element list.
 type List<'x0, 'x1> = Cons<'x0, List<'x1>>
+
 /// Shorthand for a 3-element list.
 type List<'x0, 'x1, 'x2> = Cons<'x0, List<'x1, 'x2>>
+
 /// Shorthand for a 4-element list.
 type List<'x0, 'x1, 'x2, 'x3> = Cons<'x0, List<'x1, 'x2, 'x3>>
+
 /// Shorthand for a 5-element list.
 type List<'x0, 'x1, 'x2, 'x3, 'x4> = Cons<'x0, List<'x1, 'x2, 'x3, 'x4>>
+
+/// `IsList<'xs>` is derivable when `'xs` is a proper list.
+type IsList<'xs> = | IsList
+
+/// `Append<'xs, 'ys, 'zs>` is derivable when appending `'xs` and `'ys` yields
+/// `'zs`.
+type Append<'xs, 'ys, 'zs> = | Append
 
 /// `Member<'x, 'xs>` is derivable when `'x` is a member of the list `'xs`.
 type Member<'x, 'xs> = | Member
 
-/// `Next<'x, 'y, 'xs>` is derivable when `'x` and `'y` are adjacent members of
-/// the list `'xs`.
-type Next<'x, 'y, 'xs> = | Next
+/// `Sublist<'xs, 'ys>` is derivable when `'xs` is a sublist of `'ys`.
+type Sublist<'xs, 'ys> = | Sublist
 
-/// `Right<'x, 'y, 'xs`>` is derivable when `List<'x, 'y>` is a sublist of the
+/// `Adjacent<'a, 'b, 'xs>` is derivable when `'a` and `'b` are adjacent in the
 /// list `'xs`.
-type Right<'x, 'y, 'xs> = | Right
+type Adjacent<'a, 'b, 'xs> = | Adjacent
 
 /// A few rules for deriving lists.
 type [<InferenceRules>] List () =
-  member g.Member () : Member<'x, Cons<'x, _>> = Member
-  member g.Member (_: Member<'x, 'xs>) : Member<'x, Cons<_, 'xs>> = Member
+  member g.IsList (): IsList<Nil> = IsList
+  member g.IsList (_: IsList<'xs>)
+                    : IsList<Cons<_, 'xs>> = IsList
 
-  member g.Right () : Right<'x, 'y, Cons<'x, Cons<'y, _>>> = Right
-  member g.Right (_: Right<'x, 'y, 'xs>) : Right<'x, 'y, Cons<_, 'xs>> = Right
+  member g.Append (_: IsList<'ys>)
+                    : Append<Nil, 'ys, 'ys> = Append
+  member g.Append (_: Append<'xs, 'ys, 'zs>)
+                    : Append<Cons<'x, 'xs>, 'ys, Cons<'x, 'zs>> = Append
 
-  member g.Next (_: Or<Right<'x, 'y, 'xs>,
-                       Right<'y, 'x, 'xs>>)
-                  : Next<'x, 'y, 'xs> = Next
+  member g.Sublist (_: Append<'ps, 'xs, 'qs>,
+                    _: Append<'qs, 'rs, 'ys>)
+                     : Sublist<'xs, 'ys> = Sublist
+
+  member g.Member (_: Sublist<List<'x>, 'xs>)
+                    : Member<'x, 'xs> = Member
+
+  member g.Adjacent (_: Or<Sublist<List<'a, 'b>, 'xs>,
+                           Sublist<List<'b, 'a>, 'xs>>)
+                      : Adjacent<'a, 'b, 'xs> = Adjacent
--- Examples/Toys/Zebra.fs
+++ Examples/Toys/Zebra.fs
@@ -4,7 +4,33 @@ module Toys.Zebra
 
 // Title: Finding the zebra
 //
+// https://en.wikipedia.org/wiki/Zebra_Puzzle
 // http://www.anselm.edu/internet/compsci/faculty_staff/mmalita/HOMEPAGE/logic/index.html
+//
+//  1. There are five houses.
+//  2. The Englishman lives in the red house.
+//  3. The Spaniard owns the dog.
+//  4. Coffee is drunk in the green house.
+//  5. The Ukrainian drinks tea.
+//  6. The green house is immediately to the right of the ivory house.
+//  7. The Old Gold smoker owns snails.
+//  8. Kools are smoked in the yellow house.
+//  9. Milk is drunk in the middle house.
+// 10. The Norwegian lives in the first house.
+// 11. The man who smokes Chesterfields lives in the house next to the man with
+//     the fox.
+// 12. Kools are smoked in the house next to the house where the horse is kept.
+// 13. The Lucky Strike smoker drinks orange juice.
+// 14. The Japanese smokes Parliaments.
+// 15. The Norwegian lives next to the blue house.
+//
+// Now, who drinks water?  Who owns the zebra?
+//
+// In the interest of clarity, it must be added that each of the five houses is
+// painted a different color, and their inhabitants are of different national
+// extractions, own different pets, drink different beverages and smoke
+// different brands of American cigarets [sic].  One other thing: in statement
+// 6, right means your right.
 
 open Infers
 
@@ -14,64 +40,67 @@ open Toys.Solver
 
 ////////////////////////////////////////////////////////////////////////////////
 
-type blue = class end
-type chesterfield = class end
-type coffee = class end
-type dog = class end
-type english = class end
-type fox = class end
-type green = class end
-type horse = class end
-type japonese = class end
-type juice = class end
-type kent = class end
-type kool = class end
-type lucky = class end
-type milk = class end
-type norvegian = class end
-type norwegian = class end
-type red = class end
-type snake = class end
-type spanish = class end
-type tea = class end
-type ukrainian = class end
-type water = class end
-type white = class end
-type winston = class end
-type yellow = class end
-type zebra = class end
+type Englishman = class end
+type Japanese = class end
+type Norwegian = class end
+type Spaniard = class end
+type Ukrainian = class end
+
+type Dog = class end
+type Fox = class end
+type Horse = class end
+type Snails = class end
+type Zebra = class end
+
+type Chesterfields = class end
+type Kools = class end
+type LuckyStrikes = class end
+type OldGolds = class end
+type Parliaments = class end
+
+type Coffee = class end
+type Milk = class end
+type OrangeJuice = class end
+type Tea = class end
+type Water = class end
+
+type Blue = class end
+type Green = class end
+type Ivory = class end
+type Red = class end
+type Yellow = class end
 
 /// Rules for deriving the result of the Zebra puzzle.  This is a straighforward
 /// translation of a Prolog program.
-type [<InferenceRules>] Zebra () =
+type [<InferenceRules>] Puzzle () =
   member g.Puzzle
     (_: Basic, _: List,
      _: Eq<'s, List<'x0, 'x1, 'x2, 'x3, 'x4>>,
-     _: Member<english * _ * _ * _ * red, 's>,
-     _: Member<spanish * dog * _ * _ * _, 's>,
-     _: Member<_ * _ * _ * coffee * green, 's>,
-     _: Member<ukrainian * _ * _ * tea * _, 's>,
-     _: Right<_ * _ * _ * _ * green, _ * _ * _ * _ * white, 's>,
-     _: Member<_ * snake * winston * _ * _, 's>,
-     _: Member<_ * _ * kool * _ * yellow, 's>,
-     _: Eq<'s, List<_, _, _ * _ * _ * milk * _, _, _>>,
-     _: Eq<'s, List<norwegian * _ * _ * _ * _, _, _, _, _>>,
-     _: Next<_ * _ * chesterfield * _ * _, _ * fox * _ * _ * _, 's>,
-     _: Next<_ * _ * kool * _ * _, _ * horse * _ * _ * _, 's>,
-     _: Member<_ * _ * lucky * juice * _, 's>,
-     _: Member<japonese * _ * kent * _ * _, 's>,
-     _: Next<norwegian * _ * _ * _ * _, _ * _ * _ * _ * blue, 's>,
-     _: Member<_ * _ * _ * water * _, 's>,
-     _: Member<_ * zebra * _ * _ * _, 's>)
+     _: Member<Englishman * _ * _ * _ * Red, 's>,
+     _: Member<Spaniard * Dog * _ * _ * _, 's>,
+     _: Member<_ * _ * _ * Coffee * Green, 's>,
+     _: Member<Ukrainian * _ * _ * Tea * _, 's>,
+     _: Sublist<List<_ * _ * _ * _ * Ivory, _ * _ * _ * _ * Green>, 's>,
+     _: Member<_ * Snails * OldGolds * _ * _, 's>,
+     _: Member<_ * _ * Kools * _ * Yellow, 's>,
+     _: Eq<'s, List<_, _, _ * _ * _ * Milk * _, _, _>>,
+     _: Eq<'s, List<Norwegian * _ * _ * _ * _, _, _, _, _>>,
+     _: Adjacent<_ * _ * Chesterfields * _ * _, _ * Fox * _ * _ * _, 's>,
+     _: Adjacent<_ * _ * Kools * _ * _, _ * Horse * _ * _ * _, 's>,
+     _: Member<_ * _ * LuckyStrikes * OrangeJuice * _, 's>,
+     _: Member<Japanese * _ * Parliaments * _ * _, 's>,
+     _: Adjacent<Norwegian * _ * _ * _ * _, _ * _ * _ * _ * Blue, 's>,
+     _: Member<_ * _ * _ * Water * _, 's>,
+     _: Member<_ * Zebra * _ * _ * _, 's>)
       : Result<'x0 * 'x1 * 'x2 * 'x3 * 'x4> = Result
 
 let test () : unit =
-  if Engine.tryGenerate (Solver<Zebra> ()) = Some Solution
+  if Engine.tryGenerate (Solver<Puzzle> ()) = Some Solution
   then printfn "Got solution!"
   else printfn "No solution?"
 
-// Tuple[Tuple[norwegian, fox,   kool,         water,  yellow],
-//       Tuple[ukrainian, horse, chesterfield, tea,    blue],
-//       Tuple[english,   snake, winston,      milk,   red],
-//       Tuple[japonese,  zebra, kent,         coffee, green],
-//       Tuple[spanish,   dog,   lucky,        juice,  white]]
+// Tuple[Tuple[Norwegian,  Fox,    Kools,         Water,       Yellow],
+//       Tuple[Ukrainian,  Horse,  Chesterfields, Tea,         Blue],
+//       Tuple[Englishman, Snails, OldGolds,      Milk,        Red],
+//       Tuple[Spaniard,   Dog,    LuckyStrikes,  OrangeJuice, Ivory],
+//       Tuple[Japanese,   Zebra,  Parliaments,   Coffee,      Green]]
