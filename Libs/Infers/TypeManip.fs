012a54719bee6caea9c7d429ea2cc20f64cf6365
Vesa Karvonen <vesa.a.j.k@gmail.com>
2015-10-11 18:45:53 +0300

    Drop comments.

--- Libs/Infers/TypeManip.fs
+++ Libs/Infers/TypeManip.fs
@@ -8,13 +8,10 @@ open System.Reflection
 
 ////////////////////////////////////////////////////////////////////////////////
 
-/// Represents a type constructor.  This eliminates the distinction between
-/// array types and other generic type definitions in .Net.
 type TyCon =
   | Arr of rank: int
   | Def of def: Type
 
-/// Represents a type.  This makes dealing with generic types more convenient.
 type Ty<'v> =
   | Var of 'v
   | App of TyCon * array<Ty<'v>>
@@ -36,7 +33,6 @@ type Ty<'v> =
             sprintf "%A%A" t tys
 
 module Ty =
-  /// Converts a .Net type to a `Ty`.
   let rec ofType (t: Type) =
     if t.IsArray then
       App (Arr (t.GetArrayRank ()), [|ofType (t.GetElementType ())|])
@@ -69,15 +65,11 @@ module Ty =
 
 ////////////////////////////////////////////////////////////////////////////////
 
-  /// Determines whether the given type contains type variables.  Note that this
-  /// is not given a substitution.
   let rec containsVars ty =
     match ty with
      | Var _ -> true
      | App (_, tys) -> Array.exists containsVars tys
 
-  /// Resolves the root of the type with respect to the given substition of type
-  /// variables to types.
   let rec resolveTop v2ty ty =
     match ty with
      | Var v ->
@@ -87,16 +79,12 @@ module Ty =
      | _ ->
        ty
 
-  /// Fully recursively resolves the type with respect to the given substition
-  /// of type variables to types.
   let rec resolve v2ty ty =
     match resolveTop v2ty ty with
      | Var v -> Var v
      | App (tc, tys) ->
        App (tc, tys |> Array.map (resolve v2ty))
 
-  /// Tests whether the type variable `v` occurs in the type `ty` with respect
-  /// to the given substitution of type variables to types.
   let rec private occurs v2ty v ty =
     match ty with
      | Var v' -> v = v'
@@ -129,13 +117,9 @@ module Ty =
      | _ ->
        None
 
-  /// Given two types, determines whether they can be unified and, if so,
-  /// returns substition of type variables to types.
   let tryMatch formal actual =
     tryMatchIn formal actual HashEqMap.empty
 
-  /// Given two types, determines whether they are equal, taking into
-  /// consideration the implicit universal quantification of type variables.
   let areEqual aTy bTy =
     let rec types aTy bTy v2ty =
       match (aTy, bTy) with
@@ -160,21 +144,13 @@ module Ty =
          None
     types aTy bTy HashEqMap.empty |> Option.isSome
 
-  /// Result of testing two types by `moreSpecific`.
   type MoreSpecific =
-   /// Left hand side type is more specific.
    | Lhs
-   /// Right hand side type is more specific.
    | Rhs
-   /// The two types are equivalent.
    | Equal
-   /// The types cannot be unified.
    | Unmatchable
-   /// The types unify, but neither is more specific than the other.
    | Incomparable
 
-  /// Given two types, determines whether one of the two types is clearly a more
-  /// specific type with respect to unification and returns that type if so.
   let moreSpecific lhs rhs =
     match tryMatch lhs rhs with
      | None -> Unmatchable
@@ -187,9 +163,6 @@ module Ty =
         | (false,  true) -> Rhs
         | (false, false) -> Incomparable
 
-  /// Reorder the type associations in the given array so that types that unify
-  /// with a smaller set of types are before types that unify with larger sets
-  /// of types.
   let inPlaceSelectSpecificFirst (tyrs: array<Ty<'v> * 'r>) =
     let swap i j =
       let iEl = tyrs.[i]
@@ -210,10 +183,6 @@ module Ty =
 /// `T<A, U<[B]>>`.
 type TyCursor = list<int>
 
-/// Represents a mapping of types to values of type `'r` such that given a type,
-/// the type value pairs of the mapping can be filtered based on the given type
-/// and produced in an order from more specific to less specific types with
-/// respect to unification.
 type TyTree<'r> = Lazy<TyTreeFun<'r>>
 and TyTreeFun<'r> =
   | Empty
@@ -223,10 +192,7 @@ and TyTreeFun<'r> =
               apps: HashEqMap<TyCon, TyTree<'r>> *
               vars: TyTree<'r>
 
-/// Operations on type tress.
 module TyTree =
-  /// Examines the structure of the given type to determine whether the type has
-  /// that position and, if so, returns the type at that position.
   let rec internal getAt (is: TyCursor) ty =
     match is with
      | [] ->
@@ -240,33 +206,24 @@ module TyTree =
 
   let rec private build' (ats: list<TyCursor>)
                          (tyrs: list<Ty<_> * 'r>) : TyTree<'r> =
-    // `tyrs` is the remaining association sequence we wish to classify.
     lazy match tyrs with
           | [] -> Empty
           | [(_, r)] -> One r
           | tyrs ->
-            // `ats` is a sequence of positions that we haven't yet classified
-            // types against.
             match ats with
              | [] ->
-               // No more positions to classify against, so we just create a
-               // leaf consisting of all the remaining values.
                tyrs
                |> Array.ofList
                |>!Ty.inPlaceSelectSpecificFirst
                |> Array.map snd
                |> Many
              | at::ats ->
-               // Classify types based on the position `at`.
                tyrs
                |> List.fold
                    (fun (apps, vars) (ty, r) ->
                      match getAt at ty with
                       | None ->
-                        // This cannot happen, because while building the tree,
-                        // we only produce positions we know to exist in the
-                        // types.
-                        failwith "Bug"
+                        failwith "Bug: Only existing positions are be produced?"
                       | Some (Var _) ->
                         (apps, (ty, r)::vars)
                       | Some (App (tc, args)) ->
@@ -277,52 +234,35 @@ module TyTree =
                          vars))
                    (HashEqMap.empty, [])
                |> fun (apps, vars) ->
-                  // Classify types that had a type variable at the
-                  // classification position using the remaining positions.
                   let vars = build' ats vars
-
-                  // Were some types classified distinct from others at the
-                  // position `at`?
                   if HashEqMap.isEmpty apps then
-                    // No, just produce whatever we get from classifying the
-                    // types that had a variable at the examined position.
                     force vars
                   else
-                    // Yes, so we produce a branch node.
                     (at,
                      apps
                      |> HashEqMap.map
                          (fun (n, tyds) ->
-                            // Add the new positions in these types to the
-                            // sequence of positions to classify types against.
                             let ats = List.init n (fun i -> i::at) @ ats
                             build' ats tyds),
                      vars) |> Branch
 
-  /// Builds a type tree corresponding to the given association sequence.
   let build tyrs = build' [[]] (List.ofSeq tyrs)
 
-  /// Returns a sequence values from the type tree whose associated types may
-  /// unify with the given type.
   let rec filter (formal: TyTree<'r>) (actual: Ty<_>) : seq<'r> = seq {
     match force formal with
      | Empty -> ()
      | One r -> yield r
      | Many rs -> yield! rs
      | Branch (at, apps, vars) ->
-       // Does the `actual` type have some non-var type at the branch position?
        match getAt at actual with
         | None | Some (Var _) ->
-          // No, so we can't filter out `apps` types at this branch.
           yield! HashEqMap.toSeq apps
                  |> Seq.collect (fun (_, formal) ->
                     filter formal actual)
         | Some (App (tc, _)) ->
-          // Yes, so we need only produce those `apps` that have the same tycon.
           match HashEqMap.tryFind tc apps with
            | None -> ()
            | Some formal ->
              yield! filter formal actual
-       // We also need to produce the `vars` types.
        yield! filter vars actual
   }
