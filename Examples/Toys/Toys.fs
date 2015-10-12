41387f547bb503062ffd6785b975e43629c151d3
Vesa Karvonen <vesa.a.j.k@gmail.com>
2015-10-12 10:29:22 +0300

    Move zipper tests to main module.

--- Examples/Toys/Toys.fs
+++ Examples/Toys/Toys.fs
@@ -14,6 +14,57 @@ module GMap =
     GMap.gmap ((+) 1) [("vesa", 1)] |> printfn "%A"
     GMap.gmap (fun (x: string) -> x.ToUpper ()) [("vesa", 1)] |> printfn "%A"
 
+module Zipper =
+  open Zipper
+
+  type Term =
+    | Var of string
+    | Lambda of string * Term
+    | App of Term * Term
+    | If of Term * Term * Term
+
+  let test () =
+    do let x = 1
+       printfn "%A" x
+       let z = toZipperAny x
+       printfn "%d" (getAny z |> Option.get)
+    do let x = (1, 2.0)
+       printfn "%A" x
+       let z = toZipperAny x
+       let z = z |> downHeadAny |> Option.get
+       printfn "%d" (getAny z |> Option.get)
+       let z = z |> nextAny |> Option.get
+       printfn "%f" (getAny z |> Option.get)
+    do let x = (1, 2.0, "3")
+       printfn "%A" x
+       let z = toZipperAny x
+       let z = z |> downLastAny |> Option.get
+       printfn "%s" (getAny z |> Option.get)
+       let z = z |> prevAny |> Option.get
+       printfn "%f" (getAny z |> Option.get)
+       let z = setAny 2.5 z |> Option.get
+       printfn "%f" (getAny z |> Option.get)
+       let z = z |> prevAny |> Option.get
+       printfn "%d" (getAny z |> Option.get)
+       printfn "%A" (fromZipper z)
+    do let x = App (Lambda ("x", If (Var "x", Var "a", Var "a")), Var "true")
+       printfn "%A" x
+       let z = toZipperThe x
+       printfn "%A" (getThe z)
+       let z = z |> downHeadThe |> Option.get
+       printfn "%A" (getThe z)
+       let z = z |> downHeadThe |> Option.get
+       printfn "%A" (getThe z)
+       let z = z |> downHeadThe |> Option.get
+       printfn "%A" (getThe z)
+       let z = z |> nextThe |> Option.get
+       printfn "%A" (getThe z)
+       let z = z |> nextThe |> Option.get
+       printfn "%A" (getThe z)
+       let z = z |> setAny (Var "b") |> Option.get
+       printfn "%A" (getThe z)
+       printfn "%A" (fromZipper z)
+
 [<EntryPoint>]
 let main _ =
   try
