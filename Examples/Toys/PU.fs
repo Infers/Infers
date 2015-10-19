4247411d8014bf6d9bd9aec37b307284c128811e
Vesa Karvonen <vesa.a.j.k@gmail.com>
2015-10-19 18:57:59 +0300

    Minor tweaking.

--- Examples/Toys/PU.fs
+++ Examples/Toys/PU.fs
@@ -11,103 +11,100 @@ open Toys.Rec
 
 type State = Writing | Cyclic | Acyclic
 type Info = {Pos: int64; mutable State: State}
-type PU<'x> =
-  {Pickle: Dictionary<obj, Info> -> BinaryWriter -> 'x -> unit
-   Unpickle: Dictionary<int64, obj> -> BinaryReader -> 'x}
+type PU<'x> = {P: Dictionary<obj, Info> -> BinaryWriter -> 'x -> unit
+               U: Dictionary<int64, obj> -> BinaryReader -> 'x}
 type PUE<'x> = {PU: PU<'x>}
 type PUP<'e, 'r, 'o, 't> = P of PU<'e>
 type PUS<'p, 'o, 't> = S of list<PU<'t>>
 
 type [<InferenceRules>] PU () =
   member t.Entry (_: Rep, _: Basic, _: Rec, xP) = {PU = xP}
-  member t.Int : PU<int> =
-    {Pickle = fun _ w -> w.Write
-     Unpickle = fun _ r -> r.ReadInt32 ()}
-  member t.Float : PU<float> =
-    {Pickle = fun _ w -> w.Write
-     Unpickle = fun _ r -> r.ReadDouble ()}
-  member t.String : PU<string> =
-    {Pickle = fun _ w -> w.Write
-     Unpickle = fun _ r -> r.ReadString ()}
-  member t.Elem (_: Elem<'e, 'r, 'o, 't>, eP: PU<'e>) : PUP<'e, 'r, 'o, 't> =
-    P eP
-  member t.Pair (P eP: PUP<     'e     , Pair<'e, 'r>, 'o, 't>,
-                 P rP: PUP<         'r ,          'r , 'o, 't>)
-                     : PUP<Pair<'e, 'r>, Pair<'e, 'r>, 'o, 't> =
-    P {Pickle = fun d w (Pair (e, r)) -> eP.Pickle d w e; rP.Pickle d w r
-       Unpickle = fun d r -> Pair (eP.Unpickle d r, rP.Unpickle d r)}
+
+  member t.Int = {U = fun _ r -> r.ReadInt32 ()
+                  P = fun _ w -> w.Write}
+  member t.Float = {U = fun _ r -> r.ReadDouble ()
+                    P = fun _ w -> w.Write}
+  member t.String = {U = fun _ r -> r.ReadString ()
+                     P = fun _ w -> w.Write}
+
+  member t.Elem (_: Elem<'e, 'r, 'o, 't>, ePU: PU<'e>) : PUP<'e, 'r, 'o, 't> =
+    P ePU
+
+  member t.Pair (P ePU: PUP<     'e     , Pair<'e, 'r>, 'o, 't>,
+                 P rPU: PUP<         'r ,          'r , 'o, 't>)
+                      : PUP<Pair<'e, 'r>, Pair<'e, 'r>, 'o, 't> =
+    P {P = fun d w (Pair (e, r)) -> ePU.P d w e; rPU.P d w r
+       U = fun d r -> Pair (ePU.U d r, rPU.U d r)}
+
   member t.Tuple (_: Tuple<'t>,
-                  asPairs: AsPairs<'p, 'o, 't>,
-                  P pP: PUP<'p, 'p, 'o, 't>) : PU<'t> =
-    {Pickle = fun d w -> asPairs.ToPairs >> pP.Pickle d w
-     Unpickle = fun d -> pP.Unpickle d >> asPairs.OfPairs}
-  member t.Record (record: Record<'t>,
-                   asPairs: AsPairs<'p, 'o, 't>,
-                   P pP: PUP<'p, 'p, 'o, 't>) : PU<'t> =
-    {Pickle = fun d w t ->
-      match d.TryGetValue t with
-       | (false, _) ->
-         w.Write 1uy
-         let info = {Pos = w.BaseStream.Position; State = Writing}
-         d.Add (t, info)
-         pP.Pickle d w (asPairs.ToPairs t)
-         match info.State with
-          | Acyclic -> failwith "Bug"
-          | Writing -> info.State <- Acyclic
-          | Cyclic ->
-            let pos = w.BaseStream.Position
-            w.BaseStream.Seek (info.Pos-1L, SeekOrigin.Begin) |> ignore
-            w.Write 2uy
-            w.BaseStream.Seek (pos, SeekOrigin.Begin) |> ignore
-       | (true, info) ->
-         w.Write 0uy
-         w.Write info.Pos
-         match info.State with
-          | Writing -> info.State <- Cyclic
-          | Cyclic | Acyclic -> ()
-     Unpickle = fun d r ->
+                  asP: AsPairs<'p, 'o, 't>,
+                  P pPU: PUP<'p, 'p, 'o, 't>) =
+    {P = fun d w -> asP.ToPairs >> pPU.P d w
+     U = fun d -> pPU.U d >> asP.OfPairs}
+
+  member t.Record (tR: Record<'t>,
+                   asP: AsPairs<'p, 'o, 't>,
+                   P pPU: PUP<'p, 'p, 'o, 't>) =
+    {P = fun d w t ->
+      let mutable info = Unchecked.defaultof<_>
+      if d.TryGetValue (t, &info)
+      then w.Write 0uy
+           w.Write info.Pos
+           match info.State with
+            | Writing -> info.State <- Cyclic
+            | Cyclic | Acyclic -> ()
+      else w.Write 1uy
+           info <- {Pos = w.BaseStream.Position; State = Writing}
+           d.Add (t, info)
+           asP.ToPairs t |> pPU.P d w
+           match info.State with
+            | Acyclic | Writing -> info.State <- Acyclic
+            | Cyclic ->
+              let pos = w.BaseStream.Position
+              w.BaseStream.Seek (info.Pos-1L, SeekOrigin.Begin) |> ignore
+              w.Write 2uy
+              w.BaseStream.Seek (pos, SeekOrigin.Begin) |> ignore
+     U = fun d r ->
       match r.ReadByte () with
-       | 0uy -> let pos = r.ReadInt64 () in unbox d.[pos]
+       | 0uy -> unbox d.[r.ReadInt64 ()]
        | 1uy -> let pos = r.BaseStream.Position 
-                let o = pP.Unpickle d r |> asPairs.OfPairs
+                let o = pPU.U d r |> asP.OfPairs
                 d.Add (pos, o)
                 o
        | _   -> let pos = r.BaseStream.Position
-                let o = asPairs.Default record
+                let o = asP.Default tR
                 d.Add (pos, o)
-                let mutable p = pP.Unpickle d r
-                asPairs.Overwrite (record, o, &p)
+                let mutable p = pPU.U d r
+                asP.Overwrite (tR, o, &p)
                 o}
-  member t.Case (case: Case<Empty, 'o, 't>) : PUS<Empty, 'o, 't> =
-    S [{Pickle = fun _ _ _ -> ()
-        Unpickle = fun _ _ -> case.OfPairs Unchecked.defaultof<_>}]
-  member t.Case (case: Case<'p, 'o, 't>, P pP: PUP<'p, 'p, 'o, 't>) : PUS<'p, 'o, 't> =
-    S [{Pickle = fun d w -> case.ToPairs >> pP.Pickle d w
-        Unpickle = fun d -> pP.Unpickle d >> case.OfPairs}]
-  member t.Choice (S pP: PUS<       'p     , Choice<'p, 'o>, 't>,
-                   S oP: PUS<           'o ,            'o , 't>) =
-    S (pP @ oP)        : PUS<Choice<'p, 'o>, Choice<'p, 'o>, 't>
-  member t.Sum (asChoices: AsChoices<'s, 't>, S sP: PUS<'s, 's, 't>) : PU<'t> =
-    let sP = Array.ofList sP
-    {Pickle = fun d w t ->
-       let tag = asChoices.Tag t
-       w.Write tag
-       sP.[tag].Pickle d w t
-     Unpickle = fun d r -> sP.[r.ReadInt32 ()].Unpickle d r}
 
-let pickle (x: 'x) =
-  let c = {new IEqualityComparer<obj> with
-     member t.GetHashCode (x) = LanguagePrimitives.PhysicalHash x
-     member t.Equals (l, r) = LanguagePrimitives.PhysicalEquality l r}
-  let d = Dictionary<_, _> (c)
+  member t.Case (c: Case<Empty, 'o, 't>) : PUS<Empty, 'o, 't> =
+    S [{P = fun _ _ _ -> ()
+        U = fun _ _ -> c.OfPairs Unchecked.defaultof<_>}]
+
+  member t.Case (c: Case<'p, 'o, 't>, P pPU: PUP<'p, 'p, 'o, 't>) =
+    S [{P = fun d w -> c.ToPairs >> pPU.P d w
+        U = fun d -> pPU.U d >> c.OfPairs}] : PUS<'p, 'o, 't>
+
+  member t.Choice (S pPU: PUS<       'p     , Choice<'p, 'o>, 't>,
+                   S oPU: PUS<           'o ,            'o , 't>) =
+    S <| pPU @ oPU      : PUS<Choice<'p, 'o>, Choice<'p, 'o>, 't>
+
+  member t.Sum (asC: AsChoices<'s, 't>, S sPU: PUS<'s, 's, 't>) : PU<'t> =
+    let sPU = Array.ofList sPU
+    {P = fun d w t -> let i = asC.Tag t in w.Write i ; sPU.[i].P d w t
+     U = fun d r -> sPU.[r.ReadInt32 ()].U d r}
+
+let physicalComparer = {new IEqualityComparer<obj> with
+  member t.GetHashCode (x) = LanguagePrimitives.PhysicalHash x
+  member t.Equals (l, r) = LanguagePrimitives.PhysicalEquality l r}
+
+let pickle x =
   use s = new MemoryStream ()
   use w = new BinaryWriter (s)
-  StaticRules<PU>.Generate().PU.Pickle d w x
+  StaticRules<PU>.Generate().PU.P (Dictionary (physicalComparer)) w x
   s.ToArray ()
 
-let unpickle (bs: array<byte>) : 'x =
-  let d = Dictionary<_, _> ()
-  use s = new MemoryStream (bs)
-  s.Seek (0L, SeekOrigin.Begin) |> ignore
-  use r = new BinaryReader (s)
-  StaticRules<PU>.Generate().PU.Unpickle d r
+let unpickle bytes =
+  use r = new BinaryReader (new MemoryStream (bytes, false))
+  StaticRules<PU>.Generate().PU.U (Dictionary ()) r
