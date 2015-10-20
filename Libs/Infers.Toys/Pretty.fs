e25f5c117c89f235530ff42829886ce16ea28b0f
Vesa Karvonen <vesa.a.j.k@gmail.com>
2015-10-20 20:00:29 +0300

    Pretty toy.

---
+++ Libs/Infers.Toys/Pretty.fs
@@ -0,0 +1,253 @@
+﻿// Copyright (C) by Vesa Karvonen
+
+module Infers.Toys.Pretty
+
+open Microsoft.FSharp.Reflection
+open System
+open System.Numerics
+open System.Text
+open Infers
+open Infers.Rep
+open PPrint
+
+type Pretty<'t> = 't -> Doc
+
+type Fixity =
+  | Atomic
+  | Part
+
+type [<AbstractClass>] InternalPretty<'t> () =
+  abstract Pretty: byref<'t> -> Fixity * Doc
+
+type RecPretty<'x> () =
+  inherit InternalPretty<'x> ()
+  [<DefaultValue>] val mutable impl: InternalPretty<'x>
+  override this.Pretty (x) = this.impl.Pretty (&x)
+
+type PrettyO<'t> = O of InternalPretty<'t>
+type PrettyP<'e, 'r, 'o, 't> = P of InternalPretty<'e>
+type PrettyS<'p, 'o, 't> = S of list<InternalPretty<'t>>
+
+[<AutoOpen>]
+module Util =
+  let inline atom (d: Doc) = (Atomic, d)
+  let inline part (d: Doc) = (Part, d)
+
+  let inline atxt x = atom <| txt x
+  let inline con x _ = x
+  let inline doc fn =
+    {new InternalPretty<'x> () with
+      override this.Pretty (x) =
+       fn x}
+  let inline str fn = doc (fn >> txt >> atom)
+  let inline fmt fmt = str (sprintf fmt) |> O
+  let inline atomize (f, x) =
+    match f with
+     | Atomic -> x
+     | Part -> parens x
+  let inline just (_, x) = x
+
+  let inline hexc c =
+    let c = uint32 c
+    if c < 0x10000u
+    then sprintf "\\u%04x" c
+    else sprintf "\\U%08x" c
+
+  let commaLine = comma <^> line
+  let semiLine = choice (semi <^> line) line
+
+  let inline seq (o: string)
+                 (c: string)
+                 (xP: InternalPretty<'x>)
+                 (toSeq: 'xs -> seq<'x>) =
+    let i = o.Length
+    let o = txt o
+    let c = txt c
+    O {new InternalPretty<'xs> () with
+        override this.Pretty (xs) =
+         toSeq xs
+         |> Seq.map (fun (x: 'x) ->
+              let mutable x = x
+              xP.Pretty (&x) |> snd)
+         |> joinSep semiLine
+         |> gnest i
+         |> enclose (o, c)
+         |> atom}
+
+type [<InferenceRules>] Pretty () =
+  inherit Rep ()
+
+  member t.ToPretty (O p: PrettyO<'t>) : Pretty<'t> = fun x ->
+    let mutable x = x
+    just <| p.Pretty (&x)
+
+  member t.Rec () : Rec<PrettyO<'t>> =
+    let r = RecPretty<'t> ()
+    let o = O r
+    {new Rec<PrettyO<'t>> () with
+      override t.Get () = o
+      override t.Set (O x) = r.impl <- x}
+
+  member t.Unit: PrettyO<unit> = O (doc (con (atxt "()")))
+
+  member t.Bool: PrettyO<bool> =
+    let t = atxt "true"
+    let f = atxt "false"
+    O (doc (fun b -> if b then t else f))
+
+  member t.Int8:  PrettyO<int8>  = fmt "%dy"
+  member t.Int16: PrettyO<int16> = fmt "%ds"
+  member t.Int32: PrettyO<int32> = fmt "%d"
+  member t.Int64: PrettyO<int64> = fmt "%dL"
+
+  member t.UInt8:  PrettyO<uint8>  = fmt "%duy"
+  member t.UInt16: PrettyO<uint16> = fmt "%dus"
+  member t.UInt32: PrettyO<uint32> = fmt "%du"
+  member t.UInt64: PrettyO<uint64> = fmt "%dUL"
+
+  member t.Float32: PrettyO<float32> = fmt "%.9gf" 
+  member t.Float64: PrettyO<float>   = fmt "%.17g"
+
+  member t.Char =
+    let a = atxt "'\\''"
+    let b = atxt "'\\b'"
+    let n = atxt "'\\n'"
+    let q = atxt "'\\\"'"
+    let r = atxt "'\\r'"
+    let s = atxt "'\\\\'"
+    let t = atxt "'\\t'"
+    O << doc <| function
+     | '\'' -> a | '\b' -> b | '\n' -> n | '\"' -> q | '\r' -> r | '\\' -> s
+     | '\t' -> t
+     | c when Char.IsControl c -> hexc c |> txt |> squotes |> atom
+     | c -> sprintf "'%c'" c |> atxt
+
+  member t.String: PrettyO<string> =
+    O << doc <| fun s ->
+      let mutable sawLF = false
+      let mutable sawCR = false
+      let wide =
+        let sb = StringBuilder ()
+        let inline S (s: string) = sb.Append s |> ignore
+        let inline C (c: char) = sb.Append c |> ignore
+        C '\"'
+        for c in s do
+          match c with
+           | '\'' -> S"'"
+           | '\b' -> S"\\b"
+           | '\n' -> S"\\n" ; sawLF <- true
+           | '\"' -> S"\\\""
+           | '\r' -> S"\\r" ; sawCR <- true
+           | '\\' -> S"\\\\"
+           | '\t' -> S"\\t"
+           | c when Char.IsControl c -> S (hexc c)
+           | c -> C c
+        C '\"'
+        txt (sb.ToString ())
+      let cutOnLF = sawLF
+      let inline narrow () = 
+        let parts = ResizeArray<_> ()
+        let sb = StringBuilder ()
+        let cut () =
+          parts.Add (txt (sb.ToString ()))
+          sb.Clear () |> ignore
+        let inline S (s: string) = sb.Append s |> ignore
+        let inline C (c: char) = sb.Append c |> ignore
+        C '\"'
+        for c in s do
+          match c with
+           | '\'' -> S"'"
+           | '\b' -> S"\\b"
+           | '\n' ->
+             if cutOnLF then
+               C '\\'
+               cut ()
+             S"\\n"
+           | '\"' -> S"\\\""
+           | '\r' ->
+             if not cutOnLF then
+               C '\\'
+               cut ()
+             S"\\r"
+           | '\\' -> S"\\\\"
+           | '\t' -> S"\\t"
+           | c when Char.IsControl c -> S (hexc c)
+           | c -> C c
+        C '\"'
+        cut ()
+        nest 1 (vcat parts)
+      atom (if sawLF || sawCR then choice wide (delay narrow) else wide)
+
+  member t.Option (O tP) =
+    let n = atxt "None"
+    let s = txt "Some" <^> line
+    O << doc <| function
+     | None -> n
+     | Some x ->
+       let mutable x = x
+       part (gnest 2 (s <^> atomize (tP.Pretty (&x))))
+
+  member t.Ref (O tP) =
+    let con = txt "ref" <^> line
+    O {new InternalPretty<ref<'t>> () with
+        override t.Pretty (rx) =
+         part (gnest 2 (con <^> just (tP.Pretty rx)))}
+
+  member t.List (O tP) = seq "[" "]" tP List.toSeq
+  member t.Array (O tP) = seq "[|" "|]" tP Array.toSeq
+
+  member t.Case (case: Case<Empty, 'o, 't>) : PrettyS<Empty, 'o, 't> =
+    S [doc (con (atxt case.Name))]
+
+  member t.Case (case: Case<'p, 'o, 't>,
+                 P lsP: PrettyP<'p, 'p, 'o, 't>) : PrettyS<'p, 'o, 't> =
+    let con = txt case.Name <^> line
+    S [doc <| fun x ->
+       let mutable ls = Unchecked.defaultof<_>
+       case.Extract (x, &ls)
+       part (gnest 2 (con <^> atomize (lsP.Pretty (&ls))))]
+
+  member t.Choice (S pP: PrettyS<       'p,      Choice<'p, 'o>, 't>,
+                   S oP: PrettyS<           'o ,            'o , 't>) =
+    S (pP @ oP)        : PrettyS<Choice<'p, 'o>, Choice<'p, 'o>, 't>
+
+  member t.Sum (asC: AsChoices<'s, 't>, S sP: PrettyS<'s, 's, 't>) =
+    let sP = Array.ofList sP
+    O {new InternalPretty<'t> () with
+        override t.Pretty x = sP.[asC.Tag x].Pretty (&x)}
+
+  member t.Item (_: Item<'e, 'r, 't>, O eP: PrettyO<'e>) =
+    P eP : PrettyP<'e, 'r, 't, 't>
+
+  member t.Labelled (l: Labelled<'e, 'r, 'o, 't>, O eP: PrettyO<'e>) =
+    let n = l.Name
+    if n.StartsWith "Item" &&
+       (let i = l.Index
+        i = 0 && n.Length = 4 ||
+        let suffix = string (i+1)
+        n.Length = suffix.Length + 4 &&
+        n.EndsWith suffix)
+    then P eP : PrettyP<'e, 'r, 'o, 't>
+    else let label = txt n <+> (equals <^> line)
+         P {new InternalPretty<'e> () with
+             override t.Pretty e =
+              part (gnest 2 (label <^> just (eP.Pretty (&e))))}
+
+  member t.Pair (P eP: PrettyP<     'e,      Pair<'e, 'r>, 'o, 't>,
+                 P rP: PrettyP<         'r ,          'r , 'o, 't>)
+                     : PrettyP<Pair<'e, 'r>, Pair<'e, 'r>, 'o, 't> =
+    let sep = if FSharpType.IsRecord typeof<'t> then semiLine else commaLine
+    P {new InternalPretty<Pair<'e, 'r>> () with
+        override t.Pretty ees =
+         part (just (eP.Pretty (&ees.Elem)) <^>
+               (sep <^> just (rP.Pretty (&ees.Rest))))}
+
+  member t.Product (asP: AsPairs<'p, 't, 't>, P pP: PrettyP<'p, 'p, 't, 't>) =
+    let lr = if FSharpType.IsRecord typeof<'t> then lrbrace else lrparen
+    O << doc <| fun t ->
+    let mutable es = Unchecked.defaultof<_>
+    asP.Extract (t, &es)
+    atom (enclose lr (gnest 1 (just (pP.Pretty (&es)))))
+
+let pretty x : Doc = StaticRules<Pretty>.Generate () x
+let show x = render None (pretty x)
---
+++ Libs/Infers.Toys/Pretty.fsi
@@ -0,0 +1,93 @@
+﻿// Copyright (C) by Vesa Karvonen
+
+module Infers.Toys.Pretty
+
+open System
+open System.Numerics
+open Infers
+open Infers.Rep
+open PPrint
+
+////////////////////////////////////////////////////////////////////////////////
+
+val pretty: 'x -> Doc
+
+val show: 'x -> string
+
+////////////////////////////////////////////////////////////////////////////////
+
+type Pretty<'t> = 't -> Doc
+type PrettyO<'t>
+type PrettyP<'e, 'r, 'o, 't>
+type PrettyS<'p, 'o, 't>
+
+type [<InferenceRules>] Pretty =
+  inherit Rep
+
+  new: unit -> Pretty
+
+  // ---------------------------------------------------------------------------
+
+  member ToPretty: PrettyO<'t> -> Pretty<'t>
+
+  // Rec -----------------------------------------------------------------------
+
+  member Rec: unit -> Rec<PrettyO<'t>>
+
+  // Base Types ----------------------------------------------------------------
+
+  member Unit: PrettyO<unit>
+
+  member Bool: PrettyO<bool>
+
+  member Int8: PrettyO<int8>
+  member Int16: PrettyO<int16>
+  member Int32: PrettyO<int32>
+  member Int64: PrettyO<int64>
+
+  member UInt8: PrettyO<uint8>
+  member UInt16: PrettyO<uint16>
+  member UInt32: PrettyO<uint32>
+  member UInt64: PrettyO<uint64>
+
+  member Float32: PrettyO<float32>
+  member Float64: PrettyO<float>
+
+  member Char: PrettyO<char>
+  member String: PrettyO<string>
+
+  // Special -------------------------------------------------------------------
+
+  member Option: PrettyO<'t> -> PrettyO<option<'t>>
+
+  member Ref: PrettyO<'t> -> PrettyO<ref<'t>>
+
+  member List: PrettyO<'t> -> PrettyO<list<'t>>
+
+  member Array: PrettyO<'t> -> PrettyO<array<'t>>
+
+  // Tuples and Records --------------------------------------------------------
+
+  member Item: Item<'e, 'r, 't> * PrettyO<'e> -> PrettyP<'e, 'r, 't, 't>
+
+  member Labelled: Labelled<'e, 'r, 'o, 't>
+                  * PrettyO<'e>
+                 -> PrettyP<'e, 'r, 'o, 't>
+
+  member Pair: PrettyP<     'e      , Pair<'e, 'es>, 'o, 't>
+             * PrettyP<         'es ,          'es , 'o, 't>
+            -> PrettyP<Pair<'e, 'es>, Pair<'e, 'es>, 'o, 't>
+
+  member Product: AsPairs<'p, 't, 't> * PrettyP<'p, 'p, 't, 't> -> PrettyO<'t>
+
+  // Discriminated Unions ------------------------------------------------------
+
+  member Case: Case<Empty, 'cs, 't> -> PrettyS<Empty, 'cs, 't>
+
+  member Case: Case<'p, 'o, 't> * PrettyP<'p, 'p, 'o, 't> -> PrettyS<'p, 'o, 't>
+
+  member Choice: PrettyS<       'p     , Choice<'p, 'o>, 't>
+               * PrettyS<           'o ,            'o , 't>
+              -> PrettyS<Choice<'p, 'o>, Choice<'p, 'o>, 't>
+
+  member Sum: AsChoices<'s, 't> * PrettyS<'s, 's, 't> -> PrettyO<'t>
