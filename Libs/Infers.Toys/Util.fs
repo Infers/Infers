05001a349ee7e81177b643125600dfd17a6035f8
Vesa Karvonen <vesa.a.j.k@gmail.com>
2015-11-30 19:42:46 +0200

    Whitespace.

--- Libs/Infers.Toys/Util.fs
+++ Libs/Infers.Toys/Util.fs
@@ -9,4 +9,4 @@ let physicalComparer = {new IEqualityComparer<obj> with
   member t.GetHashCode (x) = LanguagePrimitives.PhysicalHash x
   member t.Equals (l, r) = LanguagePrimitives.PhysicalEquality l r}
 
-let inline ( ^ ) x = x
+let inline (^) x = x
