57705f6e39e6d4132b518e3139ae9de6520a5f9d
Vesa Karvonen <vesa.a.j.k@gmail.com>
2016-05-24 11:19:45 +0300

    Use better method names.

--- Libs/Infers.Toys/Product.fs
+++ Libs/Infers.Toys/Product.fs
@@ -8,13 +8,13 @@ open Infers.Rep
 module Product =
   type [<Rep>] Get () =
     inherit Rules ()
-    static member Get () = id
-    static member Get (_: AsPairs<'p,'t,'t>,
-                       m: Elem<'e,'r,'t,'t>) =
+    static member Id () = id
+    static member Product (_: AsPairs<'p,'t,'t>,
+                           m: Elem<'e,'r,'t,'t>) =
       m.Get
-    static member Get (_: AsChoices<'p,'t>,
-                       _: Case<'p,'p,'t>,
-                       m: Label<'e,'r,'p,'t>) =
+    static member Union (_: AsChoices<'p,'t>,
+                         _: Case<'p,'p,'t>,
+                         m: Label<'e,'r,'p,'t>) =
       m.Get
 
   let get (t: 't) : 'e =
