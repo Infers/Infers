53c0318bbc93c21f52463d257502c66f709cebc0
Vesa Karvonen <vesa.a.j.k@gmail.com>
2016-05-24 11:27:11 +0300

    Added ad hoc product iter function.

--- Libs/Infers.Toys/Product.fs
+++ Libs/Infers.Toys/Product.fs
@@ -19,3 +19,46 @@ module Product =
 
   let get (t: 't) : 'e =
     generateDFS<Get, 't -> 'e> t
+
+  type [<AbstractClass>] Iter<'e,'r,'o,'t,'hs> () =
+    abstract Do: byref<'e> * 'hs -> unit
+
+  type [<Rep; Get>] Iter () =
+    inherit Rules ()
+
+    static member Labelled (m: Labelled<'e,'r,'o,'t>,
+                            get: 'hs -> (string -> 'e -> unit)) =
+      let n = m.Name
+      {new Iter<'e,'r,'o,'t,'hs> () with
+        member t.Do (e, hs) = get hs n e}
+
+    static member Elem (m: Elem<'e,'r,'o,'t>, get: 'hs -> (int -> 'e -> unit)) =
+      let i = m.Index
+      {new Iter<'e,'r,'o,'t,'hs> () with
+        member t.Do (e, hs) = get hs i e}
+
+    static member Elem (_: Elem<'e,'r,'o,'t>, get: 'hs -> ('e -> unit)) =
+      {new Iter<'e,'r,'o,'t,'hs> () with
+        member t.Do (e, hs) = get hs e}
+
+    static member Pair (eI: Iter<'e,Pair<'e,'r>,'o,'t,'hs>,
+                        rI: Iter<'r,        'r, 'o,'t,'hs>) =
+      {new Iter<Pair<'e,'r>,Pair<'e,'r>,'o,'t,'hs> () with
+        member t.Do (er, hs) =
+          eI.Do (&er.Elem, hs)
+          rI.Do (&er.Rest, hs)}
+
+    static member Id (get: 'hs -> ('t -> unit)) =
+      fun hs t ->
+        get hs t
+
+    static member Product (m: AsPairs<'p,'o,'t>, iter: Iter<'p,'p,'o,'t,'hs>) =
+      fun hs t ->
+        let mutable p = m.ToPairs t
+        iter.Do (&p, hs)
+
+    static member Union (_: AsChoices<'p,'t>, m: Case<'p,'p,'t>, iter: Iter<'p,'p,'p,'t,'hs>) =
+      Iter.Product (m, iter)
+
+  let iter (hs: 'hs) (p: 'p) =
+    generateDFS<Iter, 'hs -> 'p -> unit> hs p
--- Libs/Infers.Toys/Product.fsi
+++ Libs/Infers.Toys/Product.fsi
@@ -15,3 +15,25 @@ module Product =
   /// prints "a".
 #endif
   val get: 't -> 'e
+
+  /// Iterates over the elements of the product of type `'p` using the handler
+  /// functions in the product of functions of type `'handlers`.
+#if DOC
+  ///
+  /// Each handler function must have a type of one of the following forms:
+  ///
+  ///>           'e -> unit
+  ///>    int -> 'e -> unit
+  ///> string -> 'e -> unit
+  ///
+  /// Only records and single case union types have labelled elements.  The
+  /// handler is chosen by attempting to get an element of one of the above
+  /// types from the handlers product.
+  ///
+  /// For example,
+  ///
+  ///> iter (printf "%s", printf "%1.1f", printf "%d") (1, "x", 2.0)
+  ///
+  /// prints "1x2.0".
+#endif
+  val iter: 'handlers -> 'p -> unit
