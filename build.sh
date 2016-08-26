fc1d24b18ae3e71c9f0940f8cfc9d4f1016a27cd
Vesa Karvonen <vesa.a.j.k@gmail.com>
2016-08-26 09:53:17 +0300

    Updated paket

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ if hash xbuild &> /dev/null ; then
   BUILD=xbuild
   RUN=mono
 elif hash msbuild.exe &> /dev/null ; then
-  BUILD=msbuild.exe
+  BUILD="msbuild.exe /maxcpucount"
   RUN=
 else
   echo "Couldn't find build command."
@@ -13,26 +13,27 @@ fi
 
 PAKET=.paket/paket.exe
 
-if [ ! -f $PAKET ] ; then
-  $RUN .paket/paket.bootstrapper.exe
+$RUN .paket/paket.bootstrapper.exe -s --max-file-age=60
+if [ ! -x $PAKET ] ; then
+  chmod +x $PAKET
 fi
 
-$RUN $PAKET install
+$RUN $PAKET --silent restore
 
 function build () {
-  $BUILD /nologo /verbosity:quiet /p:Configuration=$2 $1
+  $BUILD /nologo /verbosity:quiet /p:Configuration="$2" "$1"
 }
 
 for SOLUTION in *.sln ; do
   if [ "$1" != "" ] ; then
-      build $SOLUTION "$*"
+      build "$SOLUTION" "$*"
   else
-    for config in Debug Release ; do
-      build $SOLUTION $config
+    for CONFIG in Debug Release ; do
+      build "$SOLUTION" $CONFIG
     done
 
-    for template in *.paket.template ; do
-      $RUN $PAKET pack output . templatefile $template
+    for TEMPLATE in *.paket.template ; do
+      $RUN $PAKET pack output . templatefile "$TEMPLATE"
     done
   fi
 done
